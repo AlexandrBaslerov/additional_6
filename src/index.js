8e5e1b39f544d01cbab010edfa710c50ba4b8d48
Alexander Baslerov <basson-777@yandex.ru>
2017-09-26 04:21:09 +0300

    additional_6/attemp_2_done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 module.exports = function zeros(expression) {
 
-  const multiply = require('./product.js');
-  
+  //const multiply = require('./product.js');
+  function multiply(first, second) {
+    
+        if (parseInt(first) === 0 || parseInt(second) === 0) {
+            return '0';
+        }
+    
+        first = first.split('').reverse();
+        second = second.split('').reverse();
+        let result = [];
+    
+        for (let i = 0; first[i] >= 0; i++) {
+            for (let j = 0; second[j] >= 0; j++) {
+                if (!result[i + j]) {
+                    result[i + j] = 0;
+                }
+    
+                result[i + j] += first[i] * second[j];
+            }
+        }
+    
+        for (let i = 0; result[i] >= 0; i++) {
+            if (result[i] >= 10) {
+                if (!result[i + 1]) {
+                    result[i + 1] = 0;
+                }
+    
+                result[i + 1] += parseInt(result[i] / 10);
+                result[i] %= 10;
+            }
+        }
+    
+        return result.reverse().join('');
+    }
   function factorial(n,countFactroial) {
       return (n != '1' && n != '0') ? multiply(n+'',factorial(n-countFactroial,countFactroial)) : '1'
     }
